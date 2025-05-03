e79f98050bd1e0e186ad7c835b22a39e28db48ed
Amir Fatahi <a.Fatahi@MN.NET>
2025-05-03 16:30:17 +0330

    chore(project): removing .gitignore from husky

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,9 +3,9 @@ import type { UserConfig } from "@commitlint/types";
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore"]],
-    "scope-case": [2, "always", "kebab-case"], // برای مثال: page یا user-profile
-    "subject-case": [0], // خاموش کردن محدودیت روی subject
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore"]], //? type of change
+    "scope-case": [2, "always", "kebab-case"], //? scop of the change
+    "subject-case": [0], //? describe whats going on
   },
 };
 
