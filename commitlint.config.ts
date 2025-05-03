5bf8ccd4f7e5e1091fee9906d6a5a0aff5ee2680
Amir Fatahi <a.Fatahi@MN.NET>
2025-05-03 16:07:22 +0330

    feat(husky): add some rules for commit message

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,11 @@ import type { UserConfig } from "@commitlint/types";
 
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore"]],
+    "scope-case": [2, "always", "kebab-case"], // برای مثال: page یا user-profile
+    "subject-case": [0], // خاموش کردن محدودیت روی subject
+  },
 };
 
 export default config;
