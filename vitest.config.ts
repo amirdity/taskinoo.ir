c6fb150bc8d2ce844d715943facaf8cfc7400fd5
Amir Fatahi <a.Fatahi@MN.NET>
2025-05-05 09:46:04 +0330

    chore(project): eslint and prettier applied

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true, // فعال کردن توابع گلوبال مانند `describe` و `it`
-    environment: "jsdom", // شبیه‌سازی محیط مرورگر (برای تست‌های React)
-    include: ["src/**/*.{test,spec}.ts", "src/**/*.{test,spec}.tsx"], // مشخص کردن مسیر فایل‌های تست
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.ts", "src/**/*.{test,spec}.tsx"],
   },
 });
