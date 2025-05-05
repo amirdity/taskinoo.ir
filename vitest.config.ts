b48bdaa32a7821179624f1a2eb9e5d9ef0beaffb
Amir Fatahi <a.Fatahi@MN.NET>
2025-05-05 09:44:52 +0330

    chore(test config): removing some comments

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true, // فعال کردن توابع گلوبال مانند `describe` و `it`
     environment: "jsdom", // شبیه‌سازی محیط مرورگر (برای تست‌های React)
-    // setupFiles: "./setupTests.ts", // مسیر فایل تنظیمات تست‌ها
+    include: ["src/**/*.{test,spec}.ts", "src/**/*.{test,spec}.tsx"], // مشخص کردن مسیر فایل‌های تست
   },
 });
