d39bdc2fbecd175a1f44352c3b0ae5a6bc3f0dec
Amir Fatahi <a.Fatahi@MN.NET>
2025-05-21 12:35:58 +0330

    fix: error fixed
    n

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,14 +3,16 @@
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        {/* <ThemeProvider attribute="class"> */}
+        {children}
+        {/* </ThemeProvider> */}
       </QueryClientProvider>
     </SessionProvider>
   );
